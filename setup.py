73ffa4c344731fe6a473409e7366db3f42505a05
Kavitha1387 <kavitha.a1387@gmail.com>
2025-07-04 16:13:10 +0530

    folder structure included

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Kavitha',
+    author_email= 'kavitha.a1387@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
